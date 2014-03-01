0d91718ad2ded548722d0136c6f3dcc53e46ba7e
bachir.assaf90 <Bachir.assaf90@gmail.com>
2014-03-01 10:49:42 +0000

    Add Branch

--- src/com/example/ontimedeliv/AddBranchActivity.java
+++ src/com/example/ontimedeliv/AddBranchActivity.java
@@ -2,21 +2,63 @@ package com.example.ontimedeliv;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 public class AddBranchActivity extends Activity {
+	
+	Button from, to;
+	int mHour,mMinute;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_branch);
-	} 
-
+		
+		from = (Button) findViewById(R.id.button2);
+		from.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				TimePickerDialog tpd = new TimePickerDialog(AddBranchActivity.this,
+						new TimePickerDialog.OnTimeSetListener() {
+				 
+				            @Override
+				            public void onTimeSet(TimePicker view, int hourOfDay,int minute) {
+				            	from.setText(hourOfDay + ":" + minute);
+				            }
+				        }, mHour, mMinute, false);
+				tpd.show();
+			
+			}
+			
+	
+		});
+		
+		to = (Button) findViewById(R.id.button1);
+		to.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				TimePickerDialog tpd = new TimePickerDialog(AddBranchActivity.this,
+						new TimePickerDialog.OnTimeSetListener() {
+				 
+				            @Override
+				            public void onTimeSet(TimePicker view, int hourOfDay,int minute) {
+				            	from.setText(hourOfDay + ":" + minute);
+				            }
+				        }, mHour, mMinute, false);
+				tpd.show();
+			
+			}
+			
+	
+		});
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.add_branch, menu);
 		return true;
 	}
-
-}
+	
+	}
