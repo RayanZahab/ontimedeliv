2a6249393acdb1f4ec3fa7c3aa0d12af8f23f22d
bachir.assaf90 <Bachir.assaf90@gmail.com>
2014-03-09 18:49:11 +0000

    -menu on long press on any list view -keep me logged in implemented

--- src/com/example/ontimedeliv/LoginActivity.java
+++ src/com/example/ontimedeliv/LoginActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -20,6 +24,7 @@ public class LoginActivity extends Activity implements OnItemSelectedListener {
 	private EditText password;
 	int counter = 3;
 	private Spinner language;
+	boolean isChecked = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +34,26 @@ public class LoginActivity extends Activity implements OnItemSelectedListener {
 		password = (EditText) findViewById(R.id.password);
 		language = (Spinner) findViewById(R.id.languageSP);
 		language.setBackgroundResource(R.drawable.myspinner);
+		CheckBox keeplog = (CheckBox) findViewById(R.id.keeploggedin);
 		//language.setOnItemSelectedListener(this);
+		
+		keeplog.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+		    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		        SharedPreferences settings = getSharedPreferences("PREFS_NAME", 0);
+		        SharedPreferences.Editor editor = settings.edit();
+		        editor.putBoolean("isChecked", isChecked);
+		        editor.commit();
+		    }
+		});
+		
+		SharedPreferences settings1 = getSharedPreferences("PREFS_NAME", 0);
+		isChecked = settings1.getBoolean("isChecked", false);
+		if (isChecked) {
+		    Intent i = new Intent(LoginActivity.this, NavigationActivity.class);
+		    startActivity(i);}
 	}
+	
 
 	@Override
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
