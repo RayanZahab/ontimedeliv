5cb33241284f4339c1807ef1e44f4df980728170
bachir.assaf90@gmail.com <bachir.assaf90@gmail.com@f429806f-9dc5-49b0-d555-10fe5ffab2ea>
2014-03-17 16:14:50 +0000

    -opening hours expendable listview -action bar return default

--- trunk/src/com/example/ontimedeliv/SplashScreenActivity.java
+++ trunk/src/com/example/ontimedeliv/SplashScreenActivity.java
@@ -1,6 +1,7 @@
 package com.example.ontimedeliv;
 
  
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +10,14 @@ import android.os.Handler;
 public class SplashScreenActivity extends Activity {
  
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2000;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
  
         new Handler().postDelayed(new Runnable() {
  
