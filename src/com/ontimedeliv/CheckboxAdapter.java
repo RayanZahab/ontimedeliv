b435195e0af9b41ac70aa554a10ddd0e6959811c
differentruna <differentruna@f429806f-9dc5-49b0-d555-10fe5ffab2ea>
2015-01-06 21:41:05 +0000

    Show phone nb in order info

--- src/com/ontimedeliv/CheckboxAdapter.java
+++ src/com/ontimedeliv/CheckboxAdapter.java
@@ -93,8 +93,9 @@ public class CheckboxAdapter extends ArrayAdapter<Item> implements Filterable {
 				convertView = vi.inflate(R.layout.category_info, null);
 				holder.picture = (ImageView) convertView
 						.findViewById(R.id.item_image);
-				String image_name = "category_"+cat.getId() + ".png";
-				new RZHelper(holder.picture, new myURL(image_name).getURL(), (Activity) context);
+				String image_name = "category_" + cat.getId() + ".png";
+				new RZHelper(holder.picture, new myURL(image_name).getURL(),
+						(Activity) context);
 			} else {
 				convertView = vi.inflate(R.layout.product_info, null);
 				holder.price = (TextView) convertView.findViewById(R.id.price);
