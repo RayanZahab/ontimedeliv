5e4377943ff3a0267867736580089ab8a1aef015
differentruna <differentruna@f429806f-9dc5-49b0-d555-10fe5ffab2ea>
2015-02-21 10:30:47 +0000

    Fixing cat images

--- trunk/src/com/ontimedeliv/Category.java
+++ trunk/src/com/ontimedeliv/Category.java
@@ -4,12 +4,14 @@ public class Category {
 	private int id, shopId;
 	private String name;
 	private boolean active;
+	private Photo photo;
 
-	public Category(int id, String name, boolean is_active, int shopId) {
+	public Category(int id, String name, boolean is_active, int shopId,Photo p) {
 		this.setId(id);
 		this.setName(name);
 		this.setActive(is_active);
 		this.setShopId(shopId);
+		this.setPhoto(p);
 	}
 
 	public Category(int id) {
@@ -50,6 +52,14 @@ public class Category {
 
 	public void setShopId(int shopId) {
 		this.shopId = shopId;
+	}
+
+	public Photo getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(Photo photo) {
+		this.photo = photo;
 	}
 
 }
